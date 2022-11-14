62d63fac119534bc1c017e6eb7ade340a13ad943
Oliver <oliver.kuhn@ancud.de>
2022-11-14 16:05:09 +0100

    Rest Service - fix; routing module integration; DB query testing

---
+++ src/angular/angularclient/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
+
+const routes: Routes = [
+  { path: 'users', component: UserListComponent },
+  { path: 'adduser', component: UserFormComponent }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+	RouterModule
+  ]
+})
+export class AppRoutingModule { }
--- src/main/java/de/twist/todo/controller/UserController.java
+++ src/main/java/de/twist/todo/controller/UserController.java
@@ -3,7 +3,6 @@ package de.twist.todo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,10 +22,15 @@ public class UserController {
     	System.out.println();
         return (List<User>) userRepository.findAll();
     }
+    
+    @GetMapping("/first-user")
+    public User getFirstUser() {
+    	System.out.println();
+        return userRepository.findAll().get(0);
+    }
 
     @PostMapping("/save-users")
     void addUser(@RequestBody User user) {
         userRepository.save(user);
     }
-    
 }
